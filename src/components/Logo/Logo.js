ad6611356386bddea91be35d0df3669964e6e24f
Ivan Samardzic <isamar01@fesb.hr>
2021-03-13 08:57:56 +0100

    Change some details in Logo compo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ const Logo = ({size}) => {
     return (
         <div className='logo-box'>
             <i style={{fontSize : size}} className="fab fa-neos"></i><span style={{fontSize : size}}>ewsLetter</span>
-            {size === '48px' && <h3 className="logo-message">Where News Becomes Reality</h3>}
+            {size === '48px' && <h3 className="logo-message">Where News Become Reality</h3>}
         </div>
     )
 }
