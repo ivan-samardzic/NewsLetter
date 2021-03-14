2e409059372007ec2b6548fa1aa959299d8e98a0
Ivan Samardzic <isamar01@fesb.hr>
2021-03-14 17:27:54 +0100

    Change some details in Logo compo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Logo.css'
+import { Link } from 'react-router-dom'
 
 const Logo = ({size}) => {
     return (
         <div className='logo-box'>
+            <Link to='/'>
             <i style={{fontSize : size}} className="fab fa-neos"></i><span style={{fontSize : size}}>ewsLetter</span>
             {size === '48px' && <h3 className="logo-message">Where News Become Reality</h3>}
+            </Link>
         </div>
     )
 }
