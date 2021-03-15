e97c539831102caf595127f73267e899d8807920
Ivan Samardzic <isamar01@fesb.hr>
2021-03-15 06:55:32 +0100

    Add Link to compos, every article is clickable and it will link user to the article content

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,27 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Article.css'
 
 const Article = ({article, type}) => {
     if(type === 'headline') {
         return (
-            <div className='article-box'>
-                <img className="article-poster" src={article.urlToImage} alt="article-poster" />
-                <div className="article-details">
-                    <div className='article-title'>{article.title}</div>
-                    <div className='article-author'>{article.author}</div>
-                    <div className='article-description'>{article.description}</div>
+            <Link to={article.url}>
+                <div className='article-box'>
+                    <img className="article-poster" src={article.urlToImage} alt="article-poster" />
+                    <div className="article-details">
+                        <div className='article-title'>{article.title}</div>
+                        <div className='article-author'>{article.author}</div>
+                        <div className='article-description'>{article.description}</div>
+                    </div>
                 </div>
-            </div>
+            </Link>
         )
     }
     else if (type === 'side'){
         return (
-            <div className='side-box'>
-                <img className="side-poster" src={article.urlToImage} alt="side-poster" />
-                <div className="side-details">
-                    <div className='side-title'>{article.title}</div>
+            <Link to={article.url}>
+                <div className='side-box'>
+                    <img className="side-poster" src={article.urlToImage} alt="side-poster" />
+                    <div className="side-details">
+                        <div className='side-title'>{article.title}</div>
+                    </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
--- src/components/FilterArticle/FilterArticle.js
+++ src/components/FilterArticle/FilterArticle.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './FilterArticle.css'
 
 const FilterArticle = ({article}) => {
     return (
-        <div className='filter-box'>
-                <img className='filter-poster' src={article.urlToImage} alt='image' />
-                <div className='filter-details'>
-                    <div className='filter-title'>{article.title}</div>
-                    <div className='filter-description'>{article.description}</div>
-                </div>
-        </div>
+        <Link to={article.url}>
+            <div className='filter-box'>
+                    <img className='filter-poster' src={article.urlToImage} alt='image' />
+                    <div className='filter-details'>
+                        <div className='filter-title'>{article.title}</div>
+                        <div className='filter-description'>{article.description}</div>
+                    </div>
+            </div>
+        </Link>
     )
 }
 
