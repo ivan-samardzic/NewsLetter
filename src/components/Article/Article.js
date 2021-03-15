0cb32afb3ee069cdc7dfc1289e74ae6faed0dc08
Ivan Samardzic <isamar01@fesb.hr>
2021-03-15 17:05:56 +0100

    Change design inside compo

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,28 +5,26 @@ import './Article.css'
 const Article = ({article, type}) => {
     if(type === 'headline') {
         return (
-            <Link to={`/${article.url}`}>
-                <div className='article-box'>
-                    <img className="article-poster" src={article.urlToImage} alt="article-poster" />
-                    <div className="article-details">
-                        <div className='article-title'>{article.title}</div>
-                        <div className='article-author'>{article.author}</div>
-                        <div className='article-description'>{article.description}</div>
-                    </div>
+            <div className='article-box'>
+                <img className="article-poster" src={article.urlToImage} alt="article-poster" />
+                <div className="article-details">
+                    <div className='article-title'>{article.title}</div>
+                    <div className='article-author'>{article.author}</div>
+                    <div className='article-description'>{article.description}</div>
+                    <button className='btn'><a href={article.url}>More</a></button>
                 </div>
-            </Link>
+            </div>
         )
     }
     else if (type === 'side'){
         return (
-            <Link to={`/${article.url}`}>
                 <div className='side-box'>
                     <img className="side-poster" src={article.urlToImage} alt="side-poster" />
                     <div className="side-details">
                         <div className='side-title'>{article.title}</div>
+                        <button className='btn'><a href={article.url}><i className="fas fa-arrow-right"></i></a></button>
                     </div>
                 </div>
-            </Link>
         )
     }
 }
